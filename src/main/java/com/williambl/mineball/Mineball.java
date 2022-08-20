c1d4f2bca14c1978697b3ad0a10ec73db9ad5393
williambl <william@williambl.com>
2022-08-21 00:45:57 +0100

    feat: add bouncing

--- src/main/java/com/williambl/mineball/Mineball.java
+++ src/main/java/com/williambl/mineball/Mineball.java
@@ -1,16 +1,27 @@
 package com.williambl.mineball;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.animal.WaterAnimal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ItemSupplier;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
@@ -32,9 +43,22 @@ public class Mineball extends Entity implements ItemSupplier {
 	@Override
 	public void tick() {
 		super.tick();
+
+		// movement + bounce
+		var movementBefore = this.getDeltaMovement();
 		this.move(MoverType.SELF, this.getDeltaMovement());
+
+		var bounce = this.getDeltaMovement().subtract(movementBefore).scale(this.getInertia());
+		var movementPlusBounce = this.getDeltaMovement().add(bounce);
+		if (movementPlusBounce.lengthSqr() <= movementBefore.lengthSqr()) { // conservation of energy
+			this.setDeltaMovement(movementPlusBounce);
+		}
+
+		// inertia + gravity
 		this.setDeltaMovement(this.getDeltaMovement().scale(this.getInertia()));
 		this.setDeltaMovement(this.getDeltaMovement().add(0.0, this.getGravity(), 0.0));
+
+		// pushing around
 		if (!this.level.isClientSide()) {
 			List<Entity> list = this.level.getEntities(this, this.getBoundingBox().inflate(0.001), EntitySelector.pushableBy(this));
 			if (!list.isEmpty()) {
